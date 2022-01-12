daf443f2864ade0fcf9af0e49bf57793673ac1f7
BaeJiYeong <jy111099@gmail.com>
2022-01-12 23:44:26 +0900

    Update format

--- 2020_C++/1697.cpp
+++ 2020_C++/1697.cpp
@@ -5,21 +5,23 @@
 
 using namespace std;
 
-bool isVisited [100001]={false,};
-int cal[3]={-1,1,2};
+bool isVisited[100001] = {
+    false,
+};
+int cal[3] = {-1, 1, 2};
 
 int main(void)
 {
-    queue <int> que;
-    queue <int> cost; 
+    queue<int> que;
+    queue<int> cost;
 
-    int N, K; 
+    int N, K;
     cin >> N >> K;
 
     que.push(N);
     cost.push(0);
 
-    while(!que.empty() && !cost.empty())
+    while (!que.empty() && !cost.empty())
     {
         int now = que.front();
         que.pop();
@@ -27,28 +29,31 @@ int main(void)
         int time = cost.front();
         cost.pop();
 
-        if(isVisited[now]) continue;
+        if (isVisited[now])
+            continue;
         isVisited[now] = true;
 
-        if(now==K)
+        if (now == K)
         {
             cout << time << "\n";
             break;
         }
- 
+
         int next = 0;
-        for(int i=0; i<3; i++)
+        for (int i = 0; i < 3; i++)
         {
-            if(cal[i]==2)
+            if (cal[i] == 2)
             {
                 next = now * cal[i];
             }
-            else{
+            else
+            {
                 next = now + cal[i];
             }
-            if(next >= 0 && next <= 100000){
+            if (next >= 0 && next <= 100000)
+            {
                 que.push(next);
-                cost.push(time+1);
+                cost.push(time + 1);
             }
         }
     }
