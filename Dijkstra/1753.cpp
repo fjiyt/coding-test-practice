65d4c14e3d62eda2e5cff5b11f64aa17df2f7f35
BaeJiYeong <jy111099@gmail.com>
2022-03-03 15:38:25 +0900

    Update 1753 problem in Dijkstra

--- Dijkstra/1753.cpp
+++ Dijkstra/1753.cpp
@@ -1,37 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#define INF 1000000
 
 using namespace std;
-
-int V, E;
-int INF = 10000000;
-
-vector<pair<int, int> > a[20002];
-int d[20002] = {
-    0,
-};
+int V,E;
+vector <pair<int,int> > adjList[20001];
+priority_queue <pair<int,int> > pq;
+int dp[20001];
 
 void dijkstra(int start)
 {
-    d[start] = 0;
+    dp[start] = 0;
+    pq.push(make_pair(0,start));
+
+    while(!pq.empty())
+    {
+        int current = pq.top().second;
+        int distance = -pq.top().first;
+        pq.pop();
+        if(distance > dp[current]) continue;
+
+        for(int i=0; i<adjList[current].size(); i++)
+        {
+            int next = adjList[current][i].first;
+            int next_distance = distance + adjList[current][i].second;
+
+            if(dp[next] > next_distance)
+            {
+                dp[next] = next_distance;
+                pq.push(make_pair(-next_distance, next));
+            }
+        }
+    }
 }
 int main()
 {
     cin >> V >> E;
     int start;
     cin >> start;
-    for (int i = 0; i < E; i++)
+
+    int u,v,w;
+    for(int i=0; i<E; i++)
     {
-        int from, to, weight;
-        cin >> from >> to >> weight;
-        a[from].push_back(make_pair(to, weight));
+        cin >> u >> v >> w;
+        adjList[u].push_back(make_pair(v,w));
     }
-    for (int i = 1; i <= E; i++)
+    for(int i=1; i<=V; i++)
     {
-        d[i] = INF;
+        dp[i] = INF;
     }
     dijkstra(start);
 
-    return 0;
+    for(int i=1; i<=V; i++)
+    {
+        if(dp[i] == INF) cout << "INF\n";
+        else cout << dp[i] << "\n";
+    }
 }
