34eb93ff0e6ccbfc4d85283907b7c8769a412345
JiYeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-04-13 23:52:05 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    
+    int idx = -1;
+    for(int i=0; i<number.size()-k; i++)
+    {
+        int max_num = 0;
+        for(int j=idx + 1; j<=i+k; j++)
+        {
+            if(max_num < number[j])
+            {
+                max_num = number[j];
+                idx = j;
+            }
+        }
+        answer += max_num;
+    }
+    return answer;
+}
