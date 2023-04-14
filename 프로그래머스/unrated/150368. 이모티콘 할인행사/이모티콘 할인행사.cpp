a6164fdac0ce4e7a9549df9f131b01482d5dfe51
JiYeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-04-14 14:07:15 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
@@ -1,71 +1,71 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
-/* 
-    1. 중복 순열으로 순서쌍 만들기
-    2. 퍼센티지 확인
-    3. 최소 금액 이상인지 확인 후 멤버십 전환 -> 값 저장
-    4. 대소 비교
-*/
+#include <cmath>
+
 using namespace std;
+int sales[] = {10,20,30,40};
+int max_sign = 0;
+int max_sale = 0;
 
-vector <vector<int>> perm;
-int percentage[4] = {10,20,30,40};
-
-void repeated_permutation(vector<int> tmp, int depth)
+void getSale(vector<vector<int>> users, vector <int> emoticons, vector <int> now)
 {
-    if(depth == tmp.size()) {
-        perm.push_back(tmp);
-        return;
-    } 
-    for(int i=0; i<4; i++)
+    int sign = 0;
+    int sale = 0;
+    for(int i=0; i<users.size(); i++)
     {
-        tmp[depth] = percentage[i];
-        repeated_permutation(tmp, depth+1);
+        int sum = 0;
+        int min_sales = users[i][0];
+        int min_money = users[i][1];
+        for(int j=0; j<emoticons.size(); j++)
+        {
+            if(min_sales > sales[now[j]]) continue;
+            sum += emoticons[j] - (emoticons[j] * sales[now[j]]/100);
+            if(sum >= min_money) {
+                sum = 0;
+                sign++;
+                break;
+            }
+        }
+        sale += sum;
+    }
+    
+    // 최댓값 갱신
+    if(max_sign < sign)
+    {
+        max_sign = sign;
+        max_sale = sale;
+    }
+    else if (max_sign==sign)
+    {
+        if(max_sale < sale)
+        {
+            max_sale = sale;
+        }
     }
 }
-
+void permutation(vector <int> emoticons, vector <int> now, vector<vector<int>> users)
+{
+    if(now.size()==emoticons.size())
+    {
+        getSale(users, emoticons, now);
+        return;
+    }
+    
+    for(int i=0; i<4; i++)
+    {
+        vector <int> tmp = now;
+        now.push_back(i);
+        permutation(emoticons, now, users);
+        now = tmp;
+    }
+}
 vector<int> solution(vector<vector<int>> users, vector<int> emoticons) {
     vector<int> answer;
-    vector <int> tmp(emoticons.size());
-    repeated_permutation(tmp, 0);
     
-    int max_money = 0;
-    int max_member = 0;
-    
-    for(int i=0; i<perm.size(); i++)
-    {
-        int member = 0;
-        int money = 0;
-        for(int j=0; j<users.size(); j++)
-        {
-            int user_money = 0;
-            for(int k=0; k<emoticons.size(); k++)
-            {
-                if(perm[i][k] < users[j][0]) continue;
-                int price = emoticons[k] * (100-perm[i][k])/100;
-                user_money += price;                
-                if(user_money >= users[j][1]) {member++; user_money = 0; break;}
-            }
-            money += user_money;
-            
-        }
-        
-        if(max_member < member)
-        {
-            max_money = money;
-            max_member = member;
-        }
-        else if (max_member == member)
-        {
-            if(max_money < money)
-            {
-                max_money = money;
-            }
-        }
-    }
-    answer.push_back(max_member);
-    answer.push_back(max_money);
+    vector <int> tmp;
+    permutation(emoticons, tmp, users);
+
+    answer.push_back(max_sign);
+    answer.push_back(max_sale);
     return answer;
 }
