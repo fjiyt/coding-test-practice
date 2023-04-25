0a0c4e2a10fdbe1abbcc28a5e04834330cde86ba
JiYeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-04-25 21:45:05 +0900

    [level 2] Title: 올바른 괄호, Time: 20.31 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,35 +3,28 @@ import java.util.Stack;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        Stack <Character> st = new Stack<> ();
+        
+        Stack <Character> stack = new Stack <> ();
         for(int i=0; i<s.length(); i++)
         {
             char c = s.charAt(i);
             if(c=='(')
             {
-                st.push(c);
+                stack.push(c);
             }
             else if(c==')')
             {
-                if(st.empty()) {
-                    answer = false;
+                if(stack.empty()) {
+                    answer=false;
                     break;
                 }
-                char tmp = st.peek();
-                st.pop();
-                if(tmp==')')
-                {
-                    answer = false;
-                    break;
-                }
+                stack.pop();
             }
         }
-        
-        if(!st.empty())
+        if(!stack.empty())
         {
             answer = false;
         }
-
         return answer;
     }
 }
