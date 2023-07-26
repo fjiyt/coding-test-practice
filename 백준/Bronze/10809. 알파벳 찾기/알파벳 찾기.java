0232ca0a65379118e30717bc4f8bb0a89438e295
Jiyeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-07-26 23:32:46 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 224 ms, Memory: 18256 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int [] location = new int[26];
+        for(int i=0; i<26; i++)
+        {
+            location[i] = -1;
+        }
+
+        for(int i=0; i<str.length(); i++)
+        {
+            int idx = str.charAt(i)-'a';
+            if(location[idx]==-1) location[idx] = i;
+        }
+
+        for(int i=0; i<26; i++)
+        {
+            System.out.print(location[i] + " ");
+        }
+        System.out.println();
+    }
+}
