1495d454100a5bb1ccab06e01971cd88e79233ae
Jiyeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-08-03 00:37:52 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.07 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,10 +1,9 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        for(int i=my_string.length()-1; i>=0; i--)
-        {
-            answer += my_string.charAt(i);
-        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
+        String answer = sb.toString();
         return answer;
     }
 }
