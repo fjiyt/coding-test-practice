27191c0f73456a774225ad5c9e16325cea413ade
Jiyeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-08-04 01:25:57 +0900

    [unrated] Title: 요격 시스템, Time: 0.44 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -2,66 +2,27 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    public class Range {
-        int s, e;
-        Range(int s, int e)
-        {
-            this.s = s;
-            this.e = e;
-        }
-    }
     public int solution(int[][] targets) {
         int answer = 0;
-        ArrayList <Range> list = new ArrayList <> ();
-        Arrays.sort(targets, new Comparator<int[]>() {
-            @Override
-            public int compare(int [] o1, int [] o2)
-            {
-                if(o1[0] == o2[0]) return o1[1] - o2[1];
-                return o1[0]-o2[0];
-            }
-        });
-        
+        Arrays.sort(targets, (o1,o2) -> (o1[0]!=o2[0] ? o1[0] - o2[0] : o1[1] - o2[1]));
+        int last = -1;
         for(int i=0; i<targets.length; i++)
         {
             int s = targets[i][0];
             int e = targets[i][1];
-            boolean isNew = true;
             
-            for(Range r : list) {
-                // list 값 갱신 -> 사이에 존재
-                if(s >= r.s && s < r.e)
-                {
-                    if(e <= r.e)
-                    {
-                        r.s = s;
-                        r.e = e;
-                    }
-                    else {
-                        r.s = s;
-                    }
-                    isNew = false;
-                }
-                else if (s<r.s)
-                {
-                    if(e < r.e && e > r.s)
-                    {
-                        r.e = e;
-                        isNew = false;
-                    }
-                    else if(e >= r.e) {
-                        isNew = false;
-                    }
-                }
-                if(!isNew) break;
+            // 새로 만드는 경우
+            if(s >= last)
+            {
+                last = e;
+                answer++;
             }
-            
-            if(isNew) {
-                list.add(new Range(s,e));
+            else if(e <= last)
+            {
+                last = e;
             }
         }
         
-        answer = list.size();
         return answer;
     }
 }
