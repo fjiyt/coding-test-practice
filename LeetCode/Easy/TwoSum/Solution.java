a53f0a37443fa66c48688ebd5e7d6c71e9020e0e
Jiyeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-08-16 22:58:31 +0900

    Create Solution.java

---
+++ LeetCode/Easy/TwoSum/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap <Integer, Integer> hm = new HashMap <> ();
+
+        for(int i=0; i<nums.length; i++)
+        {
+            int remain = target - nums[i];
+
+            if(hm.containsKey(remain)) {
+                return new int[] {hm.get(remain), i};
+            }
+            hm.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+}
