0e8ef7e7ef9a8bf5b307608079955ad0085faef8
Jiyeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-08-16 23:24:32 +0900

    Create Solution.java

---
+++ LeetCode/Easy/Valid Parentheses/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+
+    public boolean isValid(String s) {
+        Stack <Character> st = new Stack <> ();
+
+        for(int i=0; i<s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if(c=='(')
+            {
+                st.add(')');    
+            }
+            else if(c=='{')
+            {
+                st.add('}');
+            }
+            else if(c=='[')
+            {
+                st.add(']');
+            }
+            else{
+                if(st.isEmpty() || st.pop()!=c) return false;
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
