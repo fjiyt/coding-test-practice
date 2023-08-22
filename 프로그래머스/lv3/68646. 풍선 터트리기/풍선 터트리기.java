c1407309aab83eea97999b08ead6cc2167f5f658
Jiyeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-08-23 01:37:07 +0900

    [level 3] Title: 풍선 터트리기, Time: 285.84 ms, Memory: 196 MB -BaekjoonHub

--- 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,29 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] a) {
         int answer = 0;
-        int [] left = new int [a.length];
-        int [] right = new int [a.length];
         
-        left[0] = a[0];
-        for(int i=1; i<a.length; i++)
-        {
-            left[i] = Math.min(left[i-1],a[i]);
-        }
-        
-        right[a.length-1] = a[a.length-1];
-        for(int i=a.length-2; i>=0; i--)
-        {
-            right[i] = Math.min(right[i+1], a[i]);
-        }
+        HashSet <Integer> hs = new HashSet <> ();
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
         
         for(int i=0; i<a.length; i++)
         {
-            int cnt = 0;
-            if(left[i] < a[i]) cnt++;
-            if(right[i] < a[i]) cnt++;
+            min1 = Math.min(min1, a[i]);
+            min2 = Math.min(min2, a[a.length-1-i]);
             
-            if(cnt<2) answer++;
+            hs.add(min1);
+            hs.add(min2);
         }
-        return answer;
+        return hs.size();
     }
 }
