77bd4bea56c94920e136746271b220066bbc7f97
Jiyeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-09-13 14:57:09 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.java
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.java
@@ -2,35 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue <Node> pq = new PriorityQueue <> (Node::compareTo);
-        for(int i=0; i<N; i++)
-        {
+        Node[] arr = new Node[N];
+
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int t = Integer.parseInt(st.nextToken());
 
-            pq.add(new Node(a,t));
+            arr[i] = new Node(a, t);
         }
 
-        int start = 0;
-        int time = 0;
-        int now = 0;
-        while(!pq.isEmpty())
-        {
-            start = pq.peek().arrive;
-            time = pq.peek().time;
-            pq.poll();
+        Arrays.sort(arr,(n1,n2) -> {
+            if(n1.arrive == n2.arrive) return n1.time-n2.time;
+            return n1.arrive - n2.arrive;
+        });
 
-            if(now < start)
-            {
-                now = start+time;
-            }
-            else {
-                now += time;
+        int now = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (now < arr[i].arrive) {
+                now = arr[i].time + arr[i].arrive;
+            } else {
+                now += arr[i].time;
             }
         }
 
@@ -44,11 +40,5 @@ class Node{
     {
         this.arrive = arrive;
         this.time = time;
-    }
-
-    int compareTo(Node n)
-    {
-        if(this.arrive == n.arrive) return this.time - n.time;
-        return this.arrive - n.arrive;
     }
 }
