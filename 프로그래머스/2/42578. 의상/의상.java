fef4222ea13d750844b120d77415852496dd0557
Jiyeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-10-11 13:47:06 +0900

    [level 2] Title: 의상, Time: 7.56 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,22 +1,12 @@
 import java.util.*;
+import static java.util.stream.Collectors.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap <String, Integer> hm = new HashMap <> ();
-        
-        for(int i=0; i<clothes.length; i++)
-        {
-            if(hm.containsKey(clothes[i][1])) {
-                hm.put(clothes[i][1], hm.get(clothes[i][1]) + 1);
-            }
-            else hm.put(clothes[i][1], 1);
-        }
-        
-        for(Map.Entry <String, Integer> e : hm.entrySet())
-        {
-            answer *= (e.getValue() + 1);
-        }
-        return answer-1;
+        return Arrays.stream(clothes)
+            .collect(groupingBy(p->p[1], mapping(p->p[0], counting())))
+            .values()
+            .stream()
+            .collect(reducing(1L, (x,y) -> (x*(y+1)))).intValue()-1;
     }
 }
