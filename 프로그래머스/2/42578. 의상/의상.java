82406f05750d14afd7c73c4dd71ab7a4080c6920
Jiyeong Bae <46669736+fjiyt@users.noreply.github.com>
2023-10-11 13:28:29 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap <String, Integer> hm = new HashMap <> ();
+        
+        for(int i=0; i<clothes.length; i++)
+        {
+            if(hm.containsKey(clothes[i][1])) {
+                int cnt = hm.get(clothes[i][1]);
+                hm.put(clothes[i][1], cnt+1);
+            }
+            else hm.put(clothes[i][1], 1);
+        }
+        
+        for(Map.Entry <String, Integer> e : hm.entrySet())
+        {
+            answer *= (e.getValue() + 1);
+        }
+        return answer-1;
+    }
+}
